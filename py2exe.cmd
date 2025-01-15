0d2e9ebf64256d7b8c0889779c34e77fcc6bf788
EZ <31062364+EagleZhen@users.noreply.github.com>
2025-01-15 11:00:16 -0600

    might want to delete previously generated builds when needed

--- py2exe.cmd
+++ py2exe.cmd
@@ -1,3 +1,7 @@
+@REM if exist dist\*.exe del /s /q dist\*.exe
+@REM rmdir /s /q build
+@REM del /f /q *.spec
+
 pyinstaller --onefile --hidden-import plyer.platforms.win.notification --icon=icon_sleep.ico "ureply auto take attendance.py"
 pyinstaller --onefile --icon=icon_wake.ico "publish ureply answers.py"
 pyinstaller --onefile --icon=icon_penguin.ico "initialize info.py"
